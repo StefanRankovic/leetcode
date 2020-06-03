77d5dccf9b0fd192bb538369e60f8ea79a8631fd
Stefan Rankovic <stefan.rankovic.89@gmail.com>
2020-06-03 20:20:00 +0200

    update contest task

--- contest_2020_06/two_city_scheduling.py
+++ contest_2020_06/two_city_scheduling.py
@@ -2,4 +2,4 @@ class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         costs.sort(key = lambda i: i[0] - i[1])
         half = len(costs) // 2
-        return sum([i[0] for i in costs[:half]]) + sum([i[1] for i in costs[half + 1:]])
+        return sum([i[0] for i in costs[:half]]) + sum([i[1] for i in costs[half:]])
