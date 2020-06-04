793690b7ed1dcf6de1097e9579759b38cecfb045
Stefan Rankovic <stefan.rankovic.89@gmail.com>
2020-06-04 22:24:22 +0200

    new additions

--- top_interview_questions/easy/strings/reverse_string.py
+++ top_interview_questions/easy/strings/reverse_string.py
@@ -1,16 +1,5 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        if len(s) <= 1:
-            return
-        i = 0
-        k = len(s) - 1
-        while i < len(s)/2:
-            temp = s[i]
-            s[i] = s[k]
-            s[k] = temp
-            i += 1
-            k -= 1
+        for i in range(len(s)//2):
+            s[i], s[-i-1] = s[-i-1], s[i]
         
